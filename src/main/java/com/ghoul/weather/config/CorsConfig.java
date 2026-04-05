b55c4d217ef5821243b34d9cdb5ec48bce1b2a0f
Yousef El-Ghoul <yousefelghoul03@gmail.com>
2026-04-05 20:27:46 +0200

    yarab nngz 3.0

--- src/main/java/com/ghoul/weather/config/CorsConfig.java
+++ src/main/java/com/ghoul/weather/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
+        registry.addMapping("/**")
                 .allowedOrigins(allowedOrigins)
                 .allowedOriginPatterns(allowedOriginPatterns)
                 .allowedMethods("GET");
